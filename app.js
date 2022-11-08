5d4c3d5def32392738e40051f6cf8a9d8a200dd4
imidek <imidek@gmail.com>
2022-11-08 20:57:38 +0100

    recommendation page added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const btn = document.querySelector('button');
+const lists = document.querySelectorAll('li')
 
 btn.addEventListener('click', e=>{
     alert(e.target)
+})
+
+lists.forEach(list=>{
+    list.addEventListener('click',e=>{
+        console.log(e.target);
+    })
 })
